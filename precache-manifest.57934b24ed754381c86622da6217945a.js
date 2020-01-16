e1d0483dfb2537cb51dc615cd8a1dc8b631cf75d
Julieta <juliiy98@gmail.com>
2020-01-15 21:38:52 -0700

    Updates

---
+++ precache-manifest.57934b24ed754381c86622da6217945a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c65f031e19aedf2af5c5d268c6e963",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7af3947b63c9067e8512",
+    "url": "/monsters-rolodex/static/css/main.81029e1c.chunk.css"
+  },
+  {
+    "revision": "5f64445c3b396716b777",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7e82c9cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7af3947b63c9067e8512",
+    "url": "/monsters-rolodex/static/js/main.bb8e0762.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
